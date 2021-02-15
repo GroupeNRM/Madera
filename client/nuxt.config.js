8574d158c966305f21e86f10b5c85a18891d08f3
Kayoshi-dev <maxime.jme@gmail.com>
2021-02-16 00:33:06 +0100

    Fix CORS, expose project api, finish front end to create project

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -31,7 +31,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/buefy
-    'nuxt-buefy',
+    ['nuxt-buefy', {css: false}],
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
