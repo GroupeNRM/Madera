0f6e4226adbaedc804552cb6fe9f3872bbb19bd3
SmashScharrer <SmashScharrer-Dev@outlook.fr>
2021-02-16 19:59:30 +0100

    Update Login Page

--- client/pages/connexion.vue
+++ client/pages/connexion.vue
@@ -1,49 +1,108 @@
 <template>
-  <div class="container">
-    <div class="column has-text-centered">
+  <div class="columns is-vcentered">
+    <div class="column is-6">
       <img
-        src="~assets/logo_madera.svg"
-        alt="The Madera Logo!"
+        src="~assets/img/IMG_BACKGROUND_LOGIN.jpg"
+        alt="Background login page"
       >
     </div>
 
-    <form action="#" class="form-container">
-      <div class="column is-full">
-        <b-field label="E-mail">
-          <b-input type="email" placeholder="johnDoe@domain.fr" icon="email"></b-input>
-        </b-field>
-      </div>
+    <div class="column is-6">
+      <section class="section">
+        <div class="has-text-centered">
+          <img
+            src="~assets/logo_madera.svg"
+            alt="Logo Madera"
+          >
+        </div>
 
-      <div class="column is-full">
-        <b-field label="Mot de passe">
-          <b-input type="password" placeholder="************" icon="shield-key-outline" password-reveal></b-input>
+        <b-field label="E-mail" :type="validationFields.email.status" :message="validationFields.email.status === 'is-danger' ? validationFields.email.message : ''">
+          <b-input
+            v-model="email"
+            v-on:input="validationFields.email.status = ''"
+            type="email"
+            placeholder="johnDoe@domain.fr"
+            icon="email">
+          </b-input>
         </b-field>
-      </div>
 
-      <div class="column is-full">
+        <b-field label="Mot de passe" :type="validationFields.motDePasse.status" :message="validationFields.motDePasse.status === 'is-danger' ? validationFields.motDePasse.message : ''">
+          <b-input
+            type="password"
+            v:model="motDePasse"
+            v-on:input="validationFields.motDePasse.status = ''"
+            placeholder="************"
+            icon="shield-key-outline"
+            password-reveal>
+          </b-input>
+        </b-field>
+
         <b-field>
           <b-checkbox>Se souvenir de moi</b-checkbox>
         </b-field>
-      </div>
 
-      <div class="column is-full has-text-centered">
-        <b-button type="is-info" v-on:click="checkLogin" expanded>Connexion</b-button>
-      </div>
-    </form>
+        <div class="has-text-centered">
+          <b-button
+            type="is-info"
+            v-on:click="checkLogin"
+            expanded>
+            Connexion
+          </b-button>
+        </div>
+
+        <div class="has-text-centered mt-3">
+          <a
+            href="#"
+            class="is-link">
+            Pas de compte ? Inscrivez-vous
+          </a>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "connexion-page",
+  head(){
+    return {
+      title: "Connexion - Madera",
+      meta: [
+        {
+          hid: "description",
+          content: "Page de connexion à l'application Madera"
+        }
+      ]
+    }
+  },
   data(){
     return {
+      email: undefined,
+      motDePasse: undefined,
 
+      validationFields: {
+        email: {
+          status: "",
+          message: "Merci de saisir votre adresse mail"
+        },
+        motDePasse: {
+          status: "",
+          message: "Merci de saisir votre mot de passe"
+        }
+      }
     }
   },
   methods: {
     checkLogin: function(){
-      alert("CACA")
+      !this.email ? this.validationFields.email.status = "is-danger" : this.validationFields.email.status = "is-success";
+      !this.motDePasse ? this.validationFields.motDePasse.status = "is-danger" : this.validationFields.motDePasse.status = "is-success";
+
+      if(!(this.validationFields.email.status === "is-danger" || this.validationFields.motDePasse.status === "is-danger")){
+        // TODO : Nuxt Loading Start
+        // TODO : Axios GET + Traitement
+        alert("CACA");
+      }
     }
   }
 }
