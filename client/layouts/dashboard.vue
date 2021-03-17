12e2846f6a93af028ab88dc89fbf2cf83e4931a1
Kayoshi-dev <maxime.jme@gmail.com>
2021-03-17 03:13:08 +0100

    Add dashboard and sidebar, add VueChart

---
+++ client/layouts/dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Sidebar from "~/components/Sidebar";
+
+export default {
+  name: "dashboard",
+  components: {Sidebar}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/tableau-de-bord.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="is-fullheight">
+    <div class="columns is-fullheight mt-0">
+      <Sidebar/>
+      <div class="column is-9 section">
+        <h1 class="is-size-1 is-poppins has-text-weight-bold">Tableau de bord</h1>
+        <div class="columns">
+          <div class="column is-3">First</div>
+          <div class="column is-3">Second</div>
+          <div class="column is-3">Third</div>
+          <div class="column is-3">Fourth</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "tableau-de-bord",
+  middleware: 'admin-access',
+  layout: 'dashboard',
+  data() {
+    return {
+      open: true,
+      overlay: false,
+      fullheight: true,
+      fullwidth: false,
+      right: false
+    };
+  },
+  mounted() {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  section {
+    min-height: 100vh;
+  }
+
+  .is-poppins {
+    color: #2F2C37;
+    font-family: 'Varela Round', sans-serif;
+  }
+</style>
